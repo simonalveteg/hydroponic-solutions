0393ca2611ce4c8a35b2af3c4a995fd6be00a197
= <simon.alveteg@gmail.com>
2023-03-31 11:46:54 +0200

    PWM control of a pin out

--- hydroponic-solutions/hydroponic-solutions.ino
+++ hydroponic-solutions/hydroponic-solutions.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int PWM_PIN = 10;
+const int ANAL_PIN = A5;
+
+void setup() {
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(ANAL_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (int i = 255; i > 0; i--) {
+    analogWrite(PWM_PIN, i);
+    Serial.println(i);
+    delay(500);
+    i -= 9;
+  }
 }
