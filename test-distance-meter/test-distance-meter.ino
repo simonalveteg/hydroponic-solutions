07c842fc4af4017f41a20719b32db8f484267770
= <simon.alveteg@gmail.com>
2023-04-20 16:48:14 +0200

    Test avståndsmätare :)

--- test-distance-meter/test-distance-meter.ino
+++ test-distance-meter/test-distance-meter.ino
@@ -1,9 +1,20 @@
+#include <NewPing.h>
+#define TRIGGER_PIN  1  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     0  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(50);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+  Serial.print("Ping: ");
+  Serial.print(sonar.convert_cm(uS)); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+  Serial.println("cm");
 }
